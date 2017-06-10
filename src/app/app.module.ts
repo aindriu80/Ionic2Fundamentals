ecdabfeef1db7ad4e6c2b525038c6d616ce51a8c
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-06-10 11:51:18 +0100

    Platform Service - remove warnings + retrieve info on current device

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { ItemDetailsPage } from './../pages/item-details/item-details';
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicErrorHandler, IonicModule, Platform } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+
+
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
