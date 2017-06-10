3677ed5498d5fe7a30132c0261360feb07f0b031
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2017-06-10 22:24:57 +0100

    Ionic Google Maps

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -5,6 +5,11 @@ import { NavController, ModalController, Platform } from 'ionic-angular';
 import { AppVersion } from '@ionic-native/app-version';
 
 import { Camera, CameraOptions } from '@ionic-native/camera';
+
+import { Geolocation } from '@ionic-native/geolocation';
+
+import {GoogleMap} from  '@ionic-native/google-maps';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -20,9 +25,13 @@ items = [
   constructor(
     public navCtrl: NavController,
     private modalCtrl: ModalController,
+    private camera: Camera,
     platform: Platform
   ) {
       platform.ready().then(() => {
+        let map = new GoogleMap('map')
+        Geolocation.getSupportedPlatforms()
+        Camera.getSupportedPlatforms()
  console.log("Version",AppVersion.getSupportedPlatforms());
 });
   }
